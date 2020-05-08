b9ae1eb125480041e78fa62d90a6392ce86d6c34
root <reddynishu0@gmail.com>
2020-05-08 04:06:12 +0000

    first push

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 
 var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+var server = http.createServer(function (req, res) {
+   console.log('request was made:'+req.url);
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var myreadStream = fs.createReadStream(__dirname+"/index.html",'utf-8');
+  myreadStream.pipe(res);;
 }).listen(4000);
+
+conosole.log("listening on port 4000 !");
