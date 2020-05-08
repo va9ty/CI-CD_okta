dc13763713c2f5c62e7392ea15f125d6c1676cdf
root <reddynishu0@gmail.com>
2020-05-08 04:27:02 +0000

    first push

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ var server = http.createServer(function (req, res) {
    console.log('request was made:'+req.url);
   res.writeHead(200, {'Content-Type': 'text/html'});
   var myreadStream = fs.createReadStream(__dirname+"/index.html",'utf-8');
-  myreadStream.pipe(res);;
+  myreadStream.pipe(res);
+
+  console.log(req.connection.remoteÅddress);
 }).listen(4000);
 
 console.log("listening on port 4000 !");
