c6c0093656994e3ff315be9e370b0b37b4d5ef23
va9ty <reddynishu0@gmail.com>
2020-08-12 23:16:10 +0530

    Create script1.js

---
+++ static/js/script1.js
@@ -0,0 +1,12 @@
+function download()
+{
+	var d = document.getElementById("downloading");
+	d.value= "Downloading ... ";
+	
+	var i = document.getElementById("icn");
+	icn.style.display="none";
+	var t = document.getElementById("loading");
+	t.style.display="inline"; 
+}
+
+
