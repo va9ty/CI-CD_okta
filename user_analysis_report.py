3167c560675a58ff5318888302bc82d7f4f5997b
va9ty <reddynishu0@gmail.com>
2020-08-12 23:08:26 +0530

    Update user_analysis_report.py

--- user_analysis_report.py
+++ user_analysis_report.py
@@ -3,53 +3,23 @@ import requests
 from datetime import datetime
 
 from openpyxl import Workbook
+file = open("templates/temp_logs.html","w+")
 
-def process_state(users,response):
+content = " <!DOCTYPE html><html><head>"
+file.write(content)
+
+def process_state(users,response,header,choices):
 	content = [] 
 	for user in users:
 		t=user["profile"]
 		l=[]
-		try:
-			p1=t["displayName"]
-			l.append(p1)
+		for key in choices:
+			try:
+				p1=t[key]
+				l.append(p1)
 
-		except KeyError as e:
-			l.append(" ")
-		try:
-			p=t["firstName"]
-			l.append(p)
-
-		except KeyError as e:
-			l.append(" ")
-		try:
-			p1=t["lastName"]
-			l.append(p1)
-
-
-		except KeyError as e:
-			l.append(" ")
-
-		try:
-			p1=t["login"]
-			l.append(p1)
-
-		except KeyError as e:
-			print(e)
-
-		try:
-			p1=t["city"]
-			l.append(p1)
-
-		except KeyError as e:
-			l.append(" ")
-		try:
-			p1=t["countryCode"]
-			l.append(p1)
-
-
-		except KeyError as e:
-			l.append(" ")
-
+			except KeyError as e:
+				l.append(" ")
 		content.append(l)
 
 	         
@@ -69,51 +39,14 @@ def process_state(users,response):
 				for user in users:
 					t=user["profile"]
 					l=[]
-					try:
-						p1=t["displayName"]
-						l.append(p1)
+					for key in choices:
+						try:
+							p1=t[key]
+							l.append(p1)
 
-					except KeyError as e:
-						l.append(" ")
-					try:
-						p=t["firstName"]
-						l.append(p)
-
-					except KeyError as e:
-						l.append(" ")
-					try:
-						p1=t["lastName"]
-						l.append(p1)
-
-
-					except KeyError as e:
-						l.append(" ")
-
-					try:
-						p1=t["login"]
-						l.append(p1)
-
-					except KeyError as e:
-						print(e)
-
-					try:
-						p1=t["city"]
-						l.append(p1)
-
-
-					except KeyError as e:
-						l.append(" ")
-					try:
-						p1=t["countryCode"]
-						l.append(p1)
-
-
-					except KeyError as e:
-						l.append(" ")
-
-					content.append(l)
-
-				
+						except KeyError as e:
+							l.append(" ")
+					content.append(l)				
 				link_headers = response.headers["link"]
 				link_headers = list(link_headers.split(","))
 
@@ -122,15 +55,18 @@ def process_state(users,response):
 					next_link = str(link_headers[1].split(";")[0]).strip(" <>")
 				count+=1
 				print(count,response,next_link)
+				logs = "<h3>"+str(count)+" "+str(response)+" "+next_link+"</h3>"
+				file.write(logs)
 
 	return content
 
 
 
-def write_to_file(content_staged,content_active,file) : 
+def write_to_file(content_staged,content_active,content,file) : 
    
-    column_list= ["A","B","C","D","E","F"]
-    columns = ["UserName","FirstName","LastName","UPN","City","CountryCode"]
+    column_list= ["A","B","C","D","E","F","G","H","I","J","K","L","M"]
+    column_list=column_list[:len(content)]
+    columns = content
     book = Workbook()
     ws1 = book.active
     ws1 = book.create_sheet("Sheet_A",0)
@@ -174,7 +110,12 @@ def write_to_file(content_staged,content_active,file) :
     book.save(file)
 
 
-def main(Okta_tenant, API_token):
+
+
+def main(Okta_tenant, API_token,choices):
+
+	content="<h3> PROCESSING USERS IN STAGED STATE .. </h3>"
+	file.write(content)
 	print("PROCESSING USERS IN STAGED STATE")
 	token = API_token
 	final_url_staged = Okta_tenant+"/api/v1/users?filter=status eq \"STAGED\""
@@ -187,46 +128,49 @@ def main(Okta_tenant, API_token):
 
 	response_staged = requests.get(final_url_staged, headers=header)
 	users_staged = json.loads(response_staged.text)
-	#return users_staged
+	
 
-	content_staged = process_state(users_staged,response_staged)
+	content_staged = process_state(users_staged,response_staged,header,choices)
 
-
+	content="<h3> PROCESSING USERS IN ACTIVE STATE .. </h3>"
+	file.write(content)
 	print("PROCESSING USERS IN ACTIVE STATE")
 	final_url_active = Okta_tenant+"/api/v1/users?filter=status eq \"ACTIVE\""
 	response_active = requests.get(final_url_active, headers=header)
 	users_active = json.loads(response_active.text)
-	content_active = process_state(users_active,response_active)
+	content_active = process_state(users_active,response_active,header,choices)
 
-
+	content = "number of active users "+str(len(content_active))
+	file.write("<h3"+content+"</h3")
+	content = "number of staged users"+str(len(content_staged))
+	file.write("<h3"+content+"</h3")
+	
 	print("number of active users",len(content_active))
 	print("number of staged users",len(content_staged))
+	
 
-
+	
 	now = datetime.now()
 	day= now.strftime("%m-%d-%Y")
 	time = now.strftime("%H-%M-%S")
 
 
 	path = "user_analysis_report_"+day+"_"+time+".xlsx"
-	write_to_file(content_staged,content_active,path)
+	write_to_file(content_staged,content_active,choices,path)
 	temp = "written to "+path
 	
 
-	f = open('templates/thank-you.html','wb')
-
-	message = "<html><head></head><body><p>written to "+path +"</p></body></html>"
-
-	f.write(bytes(message,encoding="utf-8"))
-	f.close()
-
+	processing_state=False
+	
 	return path
+
+content = "</head><body></body></html>"
+file.write(content)
 
 
 
 #if __name__ == '__main__':
 #	main()
-
 
 
 
